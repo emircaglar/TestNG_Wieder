54e43561a376897382a8ad0b948ba6580b5100e9
lutfiyet <ltfytpcdrn@gmail.com>
2021-07-13 13:12:41 +0200

    edit successfully no r yes

--- src/_02_Tag/_02_Subcribe.java
+++ src/_02_Tag/_02_Subcribe.java
@@ -9,15 +9,22 @@ import utilities.Tools;
 
 public class _02_Subcribe extends GeneralWebDriver {
 
+    By link = By.xpath("(//a[text()='Newsletter'])[1]");
+    By cont = By.cssSelector("input[value='Continue']");
+    By yes = By.cssSelector("input[value='1']");
+    By no = By.cssSelector("input[value='0']");
+    // das ist wie POM (page object model)
+
+
     @Test
     void subcribeFunction_Yes() {
-        WebElement newsletterClick = driver.findElement(By.xpath("(//a[text()='Newsletter'])[1]"));
+        WebElement newsletterClick = driver.findElement(link);
         newsletterClick.click();
 
-        WebElement newsletterClick_Yes_button = driver.findElement(By.cssSelector("input[value='1']"));
+        WebElement newsletterClick_Yes_button = driver.findElement(yes);
         newsletterClick_Yes_button.click();
 
-        WebElement newsletterClick_radio_Continue = driver.findElement(By.cssSelector("input[value='Continue']"));
+        WebElement newsletterClick_radio_Continue = driver.findElement(cont);
         newsletterClick_radio_Continue.click();
 
         Tools.MessageBestätigung(driver);
@@ -25,13 +32,13 @@ public class _02_Subcribe extends GeneralWebDriver {
 
     @Test
     void subcribeFunction_No() {
-        WebElement newsletterClick = driver.findElement(By.xpath("(//a[text()='Newsletter'])[1]"));
+        WebElement newsletterClick = driver.findElement(link);
         newsletterClick.click();
 
-        WebElement newsletterClick_No_button = driver.findElement(By.cssSelector("input[value='0']"));
+        WebElement newsletterClick_No_button = driver.findElement(no);
         newsletterClick_No_button.click();
 
-        WebElement newsletterClick_radio_Continue = driver.findElement(By.cssSelector("input[value='Continue']"));
+        WebElement newsletterClick_radio_Continue = driver.findElement(cont);
         newsletterClick_radio_Continue.click();
 
         Tools.MessageBestätigung(driver);
@@ -42,8 +49,8 @@ public class _02_Subcribe extends GeneralWebDriver {
         WebElement newsletterClick = driver.findElement(By.xpath("(//a[text()='Newsletter'])[1]"));
         newsletterClick.click();
 
-        WebElement newsletterClick_Yes_button = driver.findElement(By.cssSelector("input[value='0']"));
-        WebElement newsletterClick_No_button = driver.findElement(By.cssSelector("input[value='1']"));
+        WebElement newsletterClick_Yes_button = driver.findElement(yes);
+        WebElement newsletterClick_No_button = driver.findElement(no);
 
         if (newsletterClick_Yes_button.isSelected())
             newsletterClick_No_button.click();
