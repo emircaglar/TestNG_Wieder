df4b2262e59432db9c4e5aa8db72588d2d1afe99
lutfiyet <ltfytpcdrn@gmail.com>
2021-07-13 13:47:09 +0200

    edit successfully dependency

---
+++ src/_03_Tag/_01_Dependency.java
@@ -0,0 +1,24 @@
+package _03_Tag;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _01_Dependency {
+    @Test
+    void startCar(){
+        System.out.println("das Auto hat funktioniert");
+        //Assert.fail();// wir schreiben so,damit der Code nict funktioniert
+    }
+    @Test(dependsOnMethods ={"startCar"} )// das ist eine Abhängigkeit, deswegen wird der Code ignoriert
+    void driveCar(){
+        System.out.println("das Auto is gegangen");
+    }
+    @Test(dependsOnMethods ={"driveCar"} )// das ist eine Abhängigkeit, deswegen wird der Code ignoriert
+    void stopCar(){
+        System.out.println("das Auto is eingehalten");
+    }
+    @Test(dependsOnMethods ={"stopCar"} ,alwaysRun = true)// der Code  funktioniert
+    void parkCar(){
+        System.out.println("das Auto wurde geparket");
+    }
+}
