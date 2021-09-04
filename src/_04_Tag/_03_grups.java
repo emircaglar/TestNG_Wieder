cac6770537ccd86d8fb871768239294272765ea2
emircaglar <emircaglar2906@gmail.com>
2021-09-05 00:00:29 +0200

    exclude include

--- src/_04_Tag/_03_grups.java
+++ src/_04_Tag/_03_grups.java
@@ -6,29 +6,33 @@ public class _03_grups {
     @Test(groups = "smoke")
     void test1()
     {
-        System.out.println("test1");
+        System.out.println("test1  smoke");
     }
     @Test(groups = "regresion")
     void test2(){
-        System.out.println("test2");
+        System.out.println("test2  regresion");
     }
     @Test(groups = "smoke")
     void test3(){
-        System.out.println("test3");
+        System.out.println("test3  smoke");
     }
     @Test(groups = "regresion")
     void test4(){
-        System.out.println("test4");
+        System.out.println("test4  regresion");
     }
     @Test(groups = "smoke")
     void test5(){
-        System.out.println("test5");
+        System.out.println("test5  smoke");
     }
     @Test(groups = "regresion")
     void test6(){
-        System.out.println("test6");
+        System.out.println("test6  regresion");
     }
 
+    @Test
+    void test7(){
+        System.out.println("test7  null");
+    }
 
 
 }
